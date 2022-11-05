bc1d8ff1af7ee30587da52e714a7f46cf3efbf2c
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-11-05 18:55:44 +0530

    python probs

---
+++ 02_pr_01_poem.py
@@ -0,0 +1,39 @@
+# Writing twinkle twinkle little star using tripple invertedd commas 
+print('''Perhaps it is little known that Twinkle, Twinkle Little Star actually consists of 5 verses, with the fifth verse rarely
+sung. Here’s the complete 5 verses, taken from the Oxford Dictionary of Nursery Rhymes (2nd edition, 1997),
+with the repetition of the first two lines added to fit the melody.
+Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+Twinkle, twinkle, little star,
+How I wonder what you are!
+****
+When the blazing sun is gone,
+When he nothing shines upon,
+Then you show your little light,
+Twinkle, twinkle, all the night.
+Twinkle, twinkle, little star,
+How I wonder what you are!
+****
+Then the traveller in the dark,
+Thanks you for your tiny spark,
+He could not see which way to go,
+If you did not twinkle so.
+Twinkle, twinkle, little star,
+How I wonder what you are!
+****
+In the dark blue sky you keep,
+And often through my curtains peep,
+For you never shut your eye,
+Till the sun is in the sky.
+Twinkle, twinkle, little star,
+How I wonder what you are!
+****
+As your bright and tiny spark,
+Lights the traveller in the dark,—
+Though I know not what you are,
+ Twinkle, twinkle, little star.
+Twinkle, twinkle, little star,
+How I wonder what you are! 
+''')
