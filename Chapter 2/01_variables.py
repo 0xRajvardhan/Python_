f46d857c7ba5ad64ccedb543d40ce51c5023cc2d
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-11-07 00:01:20 +0530

    Variables in python

---
+++ Chapter 2/01_variables.py
@@ -0,0 +1,20 @@
+# Variables in python are case sensitive 
+a = "Honey" # string 
+b = 345     # integer
+c = 45.32   # float 
+d = True
+e = None
+
+# Printing the variables 
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+# Printing the type of variables 
+print(type (a))
+print(type (b))
+print(type (c))
+print(type (d))
+print(type (e))
