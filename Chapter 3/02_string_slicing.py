2b04eb57209cfb624a05fbbee5f1d12b19ffebb1
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-11-07 15:33:08 +0530

    Slicing and indexing in python

---
+++ Chapter 3/02_string_slicing.py
@@ -0,0 +1,12 @@
+greeting = "Good morning, "
+name = "Honey"
+
+c = greeting+name
+
+print(c)
+print(name[0])    # indexing
+print(name[0:3])  # slicing
+print(name[-1])   # negative indexing
+print(name[:4])   # this is same as name[0:4]
+print(name[2:-1]) # this is negative indexing
+print(name[1:4:2])# slicing with skip value
