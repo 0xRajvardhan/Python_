f6ab40190e9d6940409a65f27c1ccfe4853de4b1
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-11-07 10:50:41 +0530

    adding two numbers in python

--- Chapter 2/04_input_function.py
+++ Chapter 2/04_input_function.py
@@ -1,5 +1,6 @@
 # This takes input as a string only 
 
-a = input("Enter your name: ")
+a = input("Enter your number: ")
 a = int(a) # if you want to convert this string input to a integer than use this 
 print(type(a))
+print(a)
