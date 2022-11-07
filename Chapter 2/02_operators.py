18d5c442dafaed16e67cea48207b0a2a765c10a7
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-11-07 09:44:03 +0530

    Operators in python

---
+++ Chapter 2/02_operators.py
@@ -0,0 +1,33 @@
+a = 3
+b = 4
+print("The value of 3+4 is:", 3+4)
+print("The value of 3-4 is:", 3-4)
+print("The value of 3*4 is:", 3*4)
+print("The value of 3/4 is:", 3/4)
+
+# assignment operators
+a = 34
+a += 2
+a -= 12
+a /= 12
+a*10000
+print(a)
+
+# comparison operators
+p = (14 >= 7)
+q = (14 <= 7)
+r = (14 < 7)
+s = (14 == 7)
+x = (14 != 7)
+print(p)
+print(q)
+print(r)
+print(s)
+print(x)
+
+# Logical operators 
+bool1 = True
+bool2 = False
+print("The value of bool1 and bool2 is", (bool1 and bool2))
+print("The value of bool1 and bool2 is", (bool1 or bool2))
+print("The value of bool1 and bool2 is", (not bool2))
