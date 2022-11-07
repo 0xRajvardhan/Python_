3a96f6fa4e473cb86441f8e27c90d83e1a2c7663
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-11-08 00:32:35 +0530

    List Methods in Python

---
+++ Chapter 4/03_list_methods.py
@@ -0,0 +1,20 @@
+L1 = [1, 8, 7, 2, 21, 15]
+print(L1)
+# sorts the list
+L1.sort()
+print(L1)
+# reverse the list 
+L1.reverse()
+print(L1)
+# adds at the end of the list 
+L1.append(45)
+print(L1)
+# inserts element at specific index
+L1.insert(2, 2345)
+print(L1)
+# removes element at a particular index 
+L1.pop(3)
+print(L1)
+# removes a particular element 
+L1.remove(21)
+print(L1)
