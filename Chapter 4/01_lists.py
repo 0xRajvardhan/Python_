d91127aa9146a97ca4b633d770fd37eb2b9f205e
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-11-08 00:15:26 +0530

    lists in python

---
+++ Chapter 4/01_lists.py
@@ -0,0 +1,17 @@
+# Create list using big brackets
+a = [1, 2, 4, 56, 6]
+print(a)
+
+print(a[0])
+print(a[2])
+# Changed the value using this
+a[0] = 98
+print(a)
+# We can create lists with items of different types
+c = [45, "Honey", False, 6.9]
+print(c)
+
+# Slicing in lists
+friends= ["Harry", "Tom", "Rohan", "Sam", "Divya", 45]
+print(friends[:4])
+print(friends[-4:])
