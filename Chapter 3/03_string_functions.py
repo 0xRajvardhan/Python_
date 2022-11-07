4b6023efc627fe7ce7ec2114abfcb59ba0d3130e
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-11-07 18:49:45 +0530

    string functions in python

---
+++ Chapter 3/03_string_functions.py
@@ -0,0 +1,10 @@
+story = "once upon a time there was a youtuber named harry who uploaded python course with notes"
+
+# string functions 
+print(len(story))
+print(story.endswith("ssdkhjcb")) #True or False
+print(story.count("a"))
+print(story.count("who"))
+print(story.capitalize())
+print(story.find("youtuber")) #first occurance
+print(story.replace("harry", "honey"))
