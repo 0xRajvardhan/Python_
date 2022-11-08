824d28818c6b751c6c41d337888d69627dae75b3
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-11-08 23:58:00 +0530

    practice que 2 of chapter 5

---
+++ Chapter 5/07_pr_02.py
@@ -0,0 +1,12 @@
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
+num3 = int(input("Enter your third number: "))
+num4 = int(input("Enter your fourth number: "))
+num5 = int(input("Enter your fifth number: "))
+num6 = int(input("Enter your sixth number: "))
+num7 = int(input("Enter your seventh number: "))
+num8 = int(input("Enter your eigth number: "))
+
+numSet = {num1, num2, num3, num4, num5, num6, num7, num8}
+
+print(numSet)
