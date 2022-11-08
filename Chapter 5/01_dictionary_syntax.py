e63b4af2de84720c47335d4813944cae8e79a04e
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-11-08 19:36:19 +0530

    dictionaries in python

---
+++ Chapter 5/01_dictionary_syntax.py
@@ -0,0 +1,15 @@
+myDict = {
+    "fast": "In a quick manner",
+    "honey": "A programer",
+    "marks": [1, 2, 5],
+    "anotherdict": {'honey': 'developer'} #nested dictionarty
+}
+'''
+dictionaries are unordered 
+dictionaries are mutable 
+keys cannot be duplicated or repeatable
+'''
+print(myDict['fast'])
+print(myDict['honey'])
+print(myDict['marks'])
+print(myDict['anotherdict']['honey'])
