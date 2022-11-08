904f9184a93f6d4e564445d425313512cc6b666c
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-11-08 12:47:49 +0530

    Fruit Store List in Python

--- Chapter 4/)5_tuple_methods.py
+++ Chapter 4/)5_tuple_methods.py
@@ -1,7 +1,7 @@
 a = (1,2,4,5)
 
-print(a.count(1)) #counts number of given element in tuple
-print(a.index(1)) #helps in searching the location index of given value print()
+print(a.count(1)) # counts number of given element in tuple
+print(a.index(1)) # helps in searching the location index of given value print()
 
 
 
---
+++ Chapter 4/06_pr_01.py
@@ -0,0 +1,10 @@
+f1 = input("Enter first fruit: ")
+f2 = input("Enter second fruit: ")
+f3 = input("Enter third fruit: ")
+f4 = input("Enter fourth fruit: ")
+f5 = input("Enter fifth fruit: ")
+f6 = input("Enter sixth fruit: ")
+f7 = input("Enter seventh fruit: ")
+
+myFruitList = (f1, f2, f3, f4, f5, f6, f7)
+print(myFruitList)
