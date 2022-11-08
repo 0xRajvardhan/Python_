5d4ce6d05a875054d69d5745c0a00a52839c6a60
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-11-08 21:40:10 +0530

    empty set in python

---
+++ Chapter 5/04_empty_set.py
@@ -0,0 +1,9 @@
+# Important: This syntax will create an empty dictionary not an empty set
+a = {}
+print(type(a))
+
+# An empty set can be created using the following syntax
+b = set()
+print(type(b))
+b.add(4)
+print(b)
