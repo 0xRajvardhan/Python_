94a5e39f0a5704dcc59a6c598677b0ed8bc1757d
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-11-08 22:09:19 +0530

    Set Methods in Python

---
+++ Chapter 5/05_set_methods.py
@@ -0,0 +1,23 @@
+# Creating a set
+b = set()
+print(type(b))
+
+# Adding
+b.add(3)
+b.add(4)
+b.add(5)
+b.add(6)
+print(b)
+# cannot add list in set because list is unhashable
+# we can also not add dictionaries for the same reason
+# but we can add tuple
+b.add((4, 5, 6))
+print(b)
+
+print(len(b))# prints number of items in "b"
+
+b.remove(3) # removes the entered element
+print(b)
+
+print(b.pop()) # removes an element randomly and "returns" it
+print(b)
