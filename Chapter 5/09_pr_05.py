043be6e7df48346aff2b5ac13a68a8b90bdfb369
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-11-09 00:27:24 +0530

    favLang pract prob

---
+++ Chapter 5/09_pr_05.py
@@ -0,0 +1,18 @@
+favLang = {
+    # "Rohan": a,
+    # "Honey": b,
+    # "Raj": c,
+    # "Harry": d
+}
+
+a = input("Enter your fav language Rohan: \n")
+b = input("Enter your fav language Honey: \n")
+c = input("Enter your fav language Raj: \n")
+d = input("Enter your fav language Harry: \n")
+
+favLang['Rohan'] = a
+favLang['Honey'] = b
+favLang['Raj'] = c
+favLang['Harry'] = d
+
+print(favLang)
