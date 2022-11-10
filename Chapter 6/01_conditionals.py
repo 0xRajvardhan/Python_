dbe4dcc7d60e498139cdf213325c9aeb4cb30ab9
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-11-11 02:27:59 +0530

    Conditionals in python

---
+++ Chapter 6/01_conditionals.py
@@ -0,0 +1,42 @@
+a = 45
+# if elif else ladder
+if (a < 3):
+
+    print("The value of a is greater than 3")
+
+elif (a < 7):
+
+    print("The value of a is greater than 7")
+
+elif (a > 13):
+
+    print("The value of a is greater than 7")
+
+elif (a > 17):
+
+    print("The value of a is greater than 17")
+
+else:
+    print("The value is not greater than 3 or 7")
+
+print("Done")
+
+# Multiple if statements
+if (a < 3):
+
+    print("The value of a is greater than 3")
+
+if (a < 7):
+
+    print("The value of a is greater than 7")
+
+if (a > 13):
+
+    print("The value of a is greater than 7")
+
+if (a > 17):
+
+    print("The value of a is greater than 17")
+
+else:
+    print("The value is not greater than 3 or 7")
