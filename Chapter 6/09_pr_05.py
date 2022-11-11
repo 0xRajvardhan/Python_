576e524b9265f9c8a582d677602578082cd1c147
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-11-12 01:51:29 +0530

    Name in the list que

---
+++ Chapter 6/09_pr_05.py
@@ -0,0 +1,7 @@
+names = ['harry', 'honey', 'shubham', 'rohan', 'rohan', 'aditi', 'rohit']
+name = input("Enter the name to check:\n")
+
+if name in names:
+    print("Your name is present in the list")
+else:
+    print("Your name is not present in the list")
