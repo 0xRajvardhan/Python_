38981d866b424f72eb13dc90b58d13383dfde560
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-11-12 01:13:24 +0530

    simple spam detection program

---
+++ Chapter 6/07_pr_03.py
@@ -0,0 +1,18 @@
+text = input("Enter the text\n\t")
+
+
+if ("make alot of money" in text):
+    spam = True
+elif ("buy now" in text):
+    spam = True
+elif ("click this" in text):
+    spam = True
+elif ("subscribe this" in text):
+    spam = True
+else:
+    spam = False
+
+if spam:
+    print("This text is spam")
+else:
+    print("This text is not spam")
