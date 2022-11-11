800af4b04d2c5922c2c0beb35df6da5dad2952a2
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-11-12 01:05:49 +0530

    practice que 1 of chap 6

--- Chapter 6/05_pr_01.py
+++ Chapter 6/05_pr_01.py
@@ -3,16 +3,6 @@ num2 = int(input("Enter your second number:\n"))
 num3 = int(input("Enter your third number:\n"))
 num4 = int(input("Enter your fourth number:\n"))
 
-if num1 > num2 > num3 > num4:
-    print(num1, " is the greatest ")
-elif num2 > num1 > num3 > num4:
-    print(num2, " is the greatest ")
-elif num3 > num1 > num2 > num4:
-    print(num3, " is the greatest ")
-else:
-    print(num4, " is the greatest ")
-
-# another method
 
 if num1 > num4:
     f1 = num1
