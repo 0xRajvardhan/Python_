593558422fba3dcb2ceb1b5980b889d33d9f8f12
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-11-12 01:47:17 +0530

    username length verify

---
+++ Chapter 6/08_pr_04.py
@@ -0,0 +1,8 @@
+username = input("Enter username:\n\t")
+
+len = len(username)
+
+if len < 10:
+    print("Contains less than 10 characters")
+else:
+    print("Does not contain less than 10 chars")
