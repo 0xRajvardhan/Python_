b3cfed39be540b5801a8594b3b29b8d156cacc10
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-11-12 10:28:12 +0530

    if elif else ladder for grading

--- Chapter 6/10_pr_06.py
+++ Chapter 6/10_pr_06.py
@@ -1,14 +1,14 @@
 marks = int(input("Enter your marks\n"))
 
-if marks >= 90:
+if (marks >= 90):
     grade = "Ex"
-elif marks >= 80:
+elif (marks >= 80):
     grade = "A"
-elif marks >= 70:
+elif (marks >= 70):
     grade = "B"
-elif marks >= 60:
+elif (marks >= 60):
     grade = "C"
-elif marks >= 50:
+elif (marks >= 50):
     grade = "D"
 else:
     grade = "F"
