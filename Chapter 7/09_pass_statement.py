f7d4583be4d57a0278eebee614d33871f75cf53b
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-11-12 21:17:59 +0530

    pass statement

---
+++ Chapter 7/09_pass_statement.py
@@ -0,0 +1,11 @@
+# pass is a null statement in python
+i = 4
+if i > 0:
+    pass  # it means do nothing if the condition is true
+print("Honey is good")
+
+# you can also use it with while loop
+while i > 6:
+    pass
+print("Honey is good")
+
