2dad5a4e4f965204ec260a4a56e647dd298579bc
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-11-12 20:46:50 +0530

    range function in python

---
+++ Chapter 7/05_range_function.py
@@ -0,0 +1,12 @@
+# prints numbers from 0 to 7
+for i in range(8):
+    print(i)
+print("\n")
+
+# if you want it to start from a partiular number so mention it like this
+for i in range(2, 8):
+    print(i)
+print("\n")
+#  if you wish to skip numbers in a regular interval you can write it like this
+for i in range(2, 8, 2):
+    print(i)
