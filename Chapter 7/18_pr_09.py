f9fc90f0bf24777b639ac7c75443cad371b1edf9
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-11-18 10:41:03 +0530

    printing hollow square of stars

---
+++ Chapter 7/18_pr_09.py
@@ -0,0 +1,10 @@
+side = int(input("Please Enter any Side of a Square  : "))
+
+print("Hollow Square Star Pattern") 
+for i in range(side):
+    for j in range(side):
+        if(i == 0 or i == side - 1 or j == 0 or j == side - 1):
+            print('*', end = '  ')
+        else:
+            print(' ', end = '  ')
+    print()
