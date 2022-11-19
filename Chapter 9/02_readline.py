e83ed55299408180c645d473b79d471cc27899a9
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-11-19 21:08:46 +0530

    readline in python

---
+++ Chapter 9/02_readline.py
@@ -0,0 +1,21 @@
+f = open('C:\\Users\\rajva\\Desktop\\Python\\Chapter 9\\sample.txt', 'r')
+# reads first line 
+data = f.readline()
+print(data)
+# reads second line 
+data = f.readline()
+print(data)
+# reads third line 
+data = f.readline()
+print(data)
+# reads fourth line 
+data = f.readline()
+print(data)
+# reads fifth line 
+data = f.readline()
+print(data)
+# reads sixth line 
+data = f.readline()
+print(data)
+
+f.close()
