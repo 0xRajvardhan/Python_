48217564962fbc549e46f804838d391b3d302854
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-11-19 23:30:25 +0530

    append

--- Chapter 9/03_write.py
+++ Chapter 9/03_write.py
@@ -1,3 +1,3 @@
-f = open('C:\\Users\\rajva\\Desktop\\Python\\Chapter 9\\another.txt', 'w')
-f.write("This is how you write it")
+f = open('C:\\Users\\rajva\\Desktop\\Python\\Chapter 9\\another.txt', 'a')
+f.write("and this is how you append")
 f.close()
