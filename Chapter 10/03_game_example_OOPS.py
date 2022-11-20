31138dd301a19e18a1e54f11102d6fec823e917c
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-11-21 00:09:47 +0530

    game example OOPS

--- Chapter 10/03_game_example_OOPS.py
+++ Chapter 10/03_game_example_OOPS.py
@@ -1,7 +1,6 @@
-
-
 class Remote:
     pass
+
 
 class Player:
     def moveRight(self):
@@ -13,8 +12,8 @@ class Player:
     def moveTop(self):
         pass
 
+
 remote1 = Remote()
 player1 = Player()
 if (remote1.isLeftPressed()):
     player1.moveLeft()
-
