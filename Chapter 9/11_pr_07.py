758adaf2e343e6030d497a61a116389b1c5ac34c
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-11-20 13:34:19 +0530

    log mining wrt lines

--- Chapter 9/11_pr_07.py
+++ Chapter 9/11_pr_07.py
@@ -6,7 +6,6 @@ with open('Chapter 9/IBM log.txt') as f:
         content = f.readline()
 
         if 'python' in content.lower():  # this lower command helps to prevent issues that get raised due to case sensitivity
-            print(content)
-            print("Yes python is present")
-            print(i)
+            print(content, end="")
+            print(f"Yes python is present on line number {i} \n")
         i += 1
