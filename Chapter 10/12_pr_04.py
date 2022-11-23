94fe233a9c69587db9c0b63e8726e15503c8c563
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-11-23 17:43:27 +0530

    adding a static method to the calculator

---
+++ Chapter 10/12_pr_04.py
@@ -0,0 +1,22 @@
+class Calculator():
+    @staticmethod
+    def greet():
+        print("Hello there welcome to calculator")
+
+    def __init__(self, num):
+        self.number = num
+
+    def square(self):
+        print(f"The value of {self.number} sqaure is {self.number ** 2}")
+
+    def squarRoot(self):
+        print(f"The square root of {self.number} is {self.number ** (0.5)}")
+
+    def cube(self):
+        print(f"The value of {self.number} sqaure is {self.number ** 3}")
+
+Calculator.greet()
+a = Calculator(3)
+a.square()
+a.squarRoot()
+a.cube()
